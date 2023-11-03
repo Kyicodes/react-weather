86f0acfeb950d9648908dd3b1fa3113a3be585f4
Kyicodes <140958930+Kyicodes@users.noreply.github.com>
2023-11-02 23:20:21 -0700

    Update open-source link

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/Kyicodes/my-weather-app">Open-source </a>by
+      <a href="https://github.com/Kyicodes/react-weather">Open-source </a>by
       Ta'kyiah Jones
     </div>
   );
